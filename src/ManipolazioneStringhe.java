e1df74f5dacfe6cfb12c7ca8dc5b0fe8ae246e3f
Alessia Tiraboschi <alessia.tiraboschi@itsrizzoli.it>
2023-12-19 11:44:37 +0100

    finale

--- src/ManipolazioneStringhe.java
+++ src/ManipolazioneStringhe.java
@@ -11,6 +11,8 @@ public class ManipolazioneStringhe {
         System.out.println("La stringa minuscola è: " + minuscolo(stringa));
         System.out.println("La stringa maiuscola è: " + maiuscolo(stringa));
         System.out.println("La iniziale della stringa è: " + iniziale(stringa));
+        System.out.println("Le finali della stringa è: " + finale(stringa));
+
 
         scanner.close();
     }
@@ -26,9 +28,12 @@ public class ManipolazioneStringhe {
     public static String maiuscolo ( String parametro1)  {
         return parametro1.toUpperCase();
     }
+
     public static char iniziale ( String parametro1)  {
         return parametro1.charAt(0);
     }
 
-
+    public static String finale ( String parametro1)  {
+        return parametro1.substring(parametro1.length()-2);
+    }
 }
