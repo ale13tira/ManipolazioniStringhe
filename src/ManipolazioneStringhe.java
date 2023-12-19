e7d7cb3ab41cab86202e719ba999d914fb2adf3a
Alessia Tiraboschi <alessia.tiraboschi@itsrizzoli.it>
2023-12-19 11:29:15 +0100

    iniziale

--- src/ManipolazioneStringhe.java
+++ src/ManipolazioneStringhe.java
@@ -10,6 +10,7 @@ public class ManipolazioneStringhe {
         System.out.println("la stinga è lunga: " + lunghezza(stringa));
         System.out.println("La stringa minuscola è: " + minuscolo(stringa));
         System.out.println("La stringa maiuscola è: " + maiuscolo(stringa));
+        System.out.println("La iniziale della stringa è: " + iniziale(stringa));
 
         scanner.close();
     }
@@ -25,5 +26,9 @@ public class ManipolazioneStringhe {
     public static String maiuscolo ( String parametro1)  {
         return parametro1.toUpperCase();
     }
+    public static char iniziale ( String parametro1)  {
+        return parametro1.charAt(0);
+    }
+
 
 }
