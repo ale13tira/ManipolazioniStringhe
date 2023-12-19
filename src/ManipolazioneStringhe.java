05edb9d44be7f94da6965522bcb6a22171d184fd
Alessia Tiraboschi <alessia.tiraboschi@itsrizzoli.it>
2023-12-19 09:58:20 +0100

    lunghezza stringa

--- src/ManipolazioneStringhe.java
+++ src/ManipolazioneStringhe.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class ManipolazioneStringhe {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     Scanner Scanner = new scanner (System.in);
+
+     System.out.println("scivi qui stringa: ");
+     int numeroN = scanner.nextInt();
     }
+
 }
