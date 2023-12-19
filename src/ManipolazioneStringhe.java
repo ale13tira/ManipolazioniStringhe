94f120206636f8f7ea9b02bad53fd3e489ab52a1
Alessia Tiraboschi <alessia.tiraboschi@itsrizzoli.it>
2023-12-19 10:28:51 +0100

    lunghezza

--- src/ManipolazioneStringhe.java
+++ src/ManipolazioneStringhe.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 
 public class ManipolazioneStringhe {
     public static void main(String[] args) throws Exception {
-     Scanner Scanner = new scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
 
-     System.out.println("scivi qui stringa: ");
-     int numeroN = scanner.nextInt();
+        System.out.println("scivi qui stringa: ");
+        String stringa = scanner.nextLine();
+
+        System.out.println("la stinga è lunga: " + lunghezza(stringa));
+
+        scanner.close();
+    }
+
+    public static int lunghezza(String parametro1) {
+        return parametro1.length();
     }
 
 }
